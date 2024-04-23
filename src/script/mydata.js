830a4c26517d56ed15c20625d470b65b4297660f
Harold Guzman <Gzmn.h10195@gmail.com>
2024-04-23 11:25:20 +0800

    dexign

---
+++ src/script/mydata.js
@@ -0,0 +1,110 @@
+
+
+let myData = {
+    actPAge: "home",
+    homeDisplay: `
+    <div class="mysumm">
+                    
+            <h1>Im a Web and Graphic Designer</h1>
+
+            <p>
+                With a passion for creativity and a keen eye for design, I specialize in crafting compelling digital experiences that resonate with audiences. From concept to execution, I thrive on bringing ideas to life through innovative web development and striking graphic design. 
+            </p>
+            <p>
+                Whether it's designing intuitive user interfaces or conceptualizing captivating visual identities, I'm dedicated to delivering solutions that not only meet but exceed expectations. Let's collaborate and elevate your online presence together.
+            </p>
+
+            <div class="btn_contact">
+                <span id="contact"> Contact Me </span>
+            </div>
+
+    </div>
+    `,
+
+
+    aboutDisplay:`
+    <div class="about_harold">
+
+            <div class="about_heaeder">
+                <h1> About Me</h1>
+                <p>Let's Make Somthing Great!</p>
+            </div>
+
+
+
+            <div class="about_content">
+                    <div class="sideabout"> 
+                            
+                        <p>
+                            Hey there! I'm Harold Guzman, a web developer and graphic designer based in Lapasan, Cagayan de Oro City. I've been passionate about coding since I taught myself HTML, JavaScript, CSS, PHP, and jQuery. With every project, I strive to blend creativity with functionality, crafting websites and designs that not only look great but also work seamlessly.
+                        </p>
+
+                        <p>
+                            My journey into the world of web development started with a curiosity to bring my ideas to life online. Over the years, I've honed my skills through hands-on experience and continuous learning. Whether it's building responsive websites or creating eye-catching graphics, I'm committed to delivering high-quality work that exceeds expectations.
+                        </p>
+
+                        <p>
+                            Outside of coding and designing, you can find me exploring new technologies, taking on creative challenges, or simply enjoying the beauty of nature. I believe in the power of collaboration and am always eager to connect with fellow creators to bring innovative ideas to fruition. Let's work together to make the web a more vibrant and engaging place!
+                        </p>
+
+                        <div class="btn_contact">
+                            <span id="contact"> Contact Me </span>
+                        </div>
+                    
+                    </div>
+                    <div class="sideabout">
+                        <h3> GET TO KNOW ME BETTER </h3>
+                        <hr>
+                        <span class="medialinks">
+        
+                            <a target="_blank" href="https://www.linkedin.com/in/harold-guzman-a35b03163/">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"  width="45" height="45" fill="currentColor"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                            </a>
+
+                            <a target="_blank" href="https://www.upwork.com/freelancers/~01ef51b1a044872b69?mp_source=share"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 641 512" width="45" height="45" fill="currentColor"><path d="M494.7 295.6c-50.3 0-83.5-38.9-92.8-53.9c11.9-95.3 46.8-125.4 92.8-125.4c45.5 0 80.9 36.4 80.9 89.7s-35.4 89.7-80.9 89.7zm0-237.8c-81.9 0-127.8 53.4-141 108.4c-14.9-28-25.9-65.5-34.5-100.3H206v141c0 51.1-23.3 89-68.8 89s-71.6-37.8-71.6-89l.5-141H.8v141c0 41.1 13.3 78.4 37.6 105.1c25 27.5 59.2 41.8 98.8 41.8c78.8 0 133.8-60.4 133.8-146.9V112.1c8.2 31.2 27.8 91.1 65.3 143.6l-35 199.4h66.4l23.1-141.3c7.6 6.3 15.7 12 24.2 17c22.2 14 47.7 21.9 73.9 22.8c0 0 4 .2 6.1 .2c81.2 0 145.9-62.9 145.9-147.8s-64.8-148.1-146-148.1z"/></svg></a>
+                                
+                            <a target="_blank" href="https://www.onlinejobs.ph/jobseekers/info/2645779">
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="40" height="40" fill="currentColor" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
+                            <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="currentColor" stroke="none">
+                            <path d="M0 2560 l0 -2560 2560 0 2560 0 0 2560 0 2560 -2560 0 -2560 0 0 -2560z m2885 2095 c140 -21 278 -60 362 -101 176 -87 263 -254 263 -501 0 -169 -40 -284 -130 -374 -51 -51 -84 -69 -127 -69 -25 0 -24 1 17 41 61 59 100 148 100 227 0 60 -2 66 -35 99 -33 33 -39 35 -77 29 -55 -9 -102 -39 -129 -82 -25 -41 -20 -57 5 -18 10 14 42 37 72 51 47 21 57 23 81 11 38 -18 49 -64 34 -133 -16 -71 -59 -128 -125 -165 -60 -34 -56 -24 -76 -194 -6 -49 -7 -92 -3 -97 4 -4 19 -9 33 -11 20 -2 26 -10 28 -33 l3 -30 92 0 c76 0 100 -4 139 -23 60 -30 136 -98 184 -165 21 -29 59 -75 85 -103 91 -98 148 -204 213 -391 24 -70 55 -156 70 -191 l27 -63 -28 -51 c-79 -145 -328 -316 -629 -432 -109 -43 -109 -44 -10 -274 l81 -190 40 32 c116 92 261 264 334 397 21 40 50 76 71 90 126 86 240 213 293 329 37 80 34 101 -33 302 -59 177 -139 345 -230 482 -36 56 -79 124 -95 151 -57 100 -128 199 -196 270 -38 40 -69 76 -69 80 0 4 16 26 36 48 44 52 90 145 106 215 7 28 16 52 20 52 14 0 159 -143 223 -220 185 -223 310 -483 372 -779 23 -111 27 -150 27 -326 1 -126 -4 -228 -12 -275 -118 -659 -526 -1154 -1142 -1383 -274 -102 -635 -132 -914 -75 -64 12 -54 -13 -86 227 -12 84 -20 155 -18 156 2 1 44 -8 95 -21 131 -33 306 -47 435 -35 98 10 277 43 298 56 11 7 -19 88 -62 165 -17 30 -40 73 -52 94 -57 106 -154 175 -296 213 -83 23 -409 25 -544 4 l-89 -13 7 -127 c9 -167 40 -375 71 -485 32 -109 30 -134 -13 -162 -58 -39 -141 -46 -497 -42 -336 4 -375 8 -375 42 0 25 57 63 124 85 69 22 316 127 328 139 3 4 -9 81 -27 172 -77 376 -123 695 -110 771 7 42 25 62 78 84 72 30 215 13 473 -55 131 -35 130 -35 143 -11 9 15 0 25 -60 66 -38 26 -96 76 -128 111 -70 77 -179 228 -187 260 -7 29 33 118 91 199 52 74 193 210 269 261 31 20 56 40 56 44 0 14 83 101 110 115 36 18 251 73 323 81 l57 7 0 59 c0 51 3 61 20 65 17 4 20 14 20 53 l0 48 -72 -5 c-41 -3 -98 0 -130 6 -62 13 -168 62 -189 87 -18 22 -14 246 6 364 23 132 69 265 127 366 l49 86 -36 39 c-50 55 -48 90 8 143 41 39 42 40 45 16 8 -73 41 -121 99 -145 20 -8 37 -15 39 -15 2 0 -16 20 -40 43 -54 55 -72 103 -56 152 28 86 138 111 345 80z m-670 -412 c-30 -70 -66 -184 -85 -268 -11 -55 -23 -101 -26 -104 -2 -2 -44 -21 -93 -42 -433 -181 -752 -581 -837 -1049 -32 -176 -19 -476 27 -605 6 -16 15 -93 20 -170 5 -89 27 -244 59 -424 27 -157 48 -286 46 -288 -8 -9 -153 160 -216 253 -356 523 -407 1184 -135 1749 223 464 640 808 1137 939 113 30 112 30 103 9z"/>
+                            <path d="M3347 2785 c-72 -77 -159 -218 -198 -322 -24 -68 -33 -109 -37 -180 -4 -88 -3 -91 19 -103 13 -7 38 -26 56 -42 l32 -31 68 38 c46 26 100 70 168 140 55 55 120 117 144 136 l43 36 -45 59 c-65 86 -114 166 -154 250 -19 41 -37 74 -40 74 -2 0 -27 -25 -56 -55z"/>
+                            <path d="M2265 2752 c-9 -30 -114 -130 -176 -168 l-58 -36 35 -30 c19 -16 47 -58 63 -92 32 -67 86 -146 100 -146 21 0 44 168 45 327 1 90 0 163 -1 163 -1 0 -4 -8 -8 -18z"/>
+                            </g>
+                            </svg>
+                            </a>
+
+                            <a target="_blank" href="https://www.behance.net/haroldguzman3">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="45" height="45" fill="currentColor"> <path d="M155.3 318.4c17.2 0 31.2-6.1 31.2-25.4c0-19.7-11.7-27.4-30.3-27.5h-46v52.9h45.1zm-5.4-129.6H110.3v44.8H153c15.1 0 25.8-6.6 25.8-22.9c0-17.7-13.7-21.9-28.9-21.9zm129.5 74.8h62.2c-1.7-18.5-11.3-29.7-30.5-29.7c-18.3 0-30.5 11.4-31.7 29.7zM384 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64zM349.5 185H271.7V166.1h77.8V185zM193.7 243.7c23.6 6.7 35 27.5 35 51.6c0 39-32.7 55.7-67.6 55.9H68v-192h90.5c32.9 0 61.4 9.3 61.4 47.5c0 19.3-9 28.8-26.2 37zm118.7-38.6c43.5 0 67.6 34.3 67.6 75.4c0 1.6-.1 3.3-.2 5c0 .8-.1 1.5-.1 2.2H279.5c0 22.2 11.7 35.3 34.1 35.3c11.6 0 26.5-6.2 30.2-18.1h33.7c-10.4 31.9-31.9 46.8-65.1 46.8c-43.8 0-71.1-29.7-71.1-73c0-41.8 28.7-73.6 71.1-73.6z"/></svg>
+                            </a>
+
+
+                            <a target="_blank"  href="https://github.com/HLord-G">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="45" height="45" fill="currentColor"><path d="M448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM265.8 407.7c0-1.8 0-6 .1-11.6c.1-11.4 .1-28.8 .1-43.7c0-15.6-5.2-25.5-11.3-30.7c37-4.1 76-9.2 76-73.1c0-18.2-6.5-27.3-17.1-39c1.7-4.3 7.4-22-1.7-45c-13.9-4.3-45.7 17.9-45.7 17.9c-13.2-3.7-27.5-5.6-41.6-5.6s-28.4 1.9-41.6 5.6c0 0-31.8-22.2-45.7-17.9c-9.1 22.9-3.5 40.6-1.7 45c-10.6 11.7-15.6 20.8-15.6 39c0 63.6 37.3 69 74.3 73.1c-4.8 4.3-9.1 11.7-10.6 22.3c-9.5 4.3-33.8 11.7-48.3-13.9c-9.1-15.8-25.5-17.1-25.5-17.1c-16.2-.2-1.1 10.2-1.1 10.2c10.8 5 18.4 24.2 18.4 24.2c9.7 29.7 56.1 19.7 56.1 19.7c0 9 .1 21.7 .1 30.6c0 4.8 .1 8.6 .1 10c0 4.3-3 9.5-11.5 8C106 393.6 59.8 330.8 59.8 257.4c0-91.8 70.2-161.5 162-161.5s166.2 69.7 166.2 161.5c.1 73.4-44.7 136.3-110.7 158.3c-8.4 1.5-11.5-3.7-11.5-8zm-90.5-54.8c-.2-1.5 1.1-2.8 3-3.2c1.9-.2 3.7 .6 3.9 1.9c.3 1.3-1 2.6-3 3c-1.9 .4-3.7-.4-3.9-1.7zm-9.1 3.2c-2.2 .2-3.7-.9-3.7-2.4c0-1.3 1.5-2.4 3.5-2.4c1.9-.2 3.7 .9 3.7 2.4c0 1.3-1.5 2.4-3.5 2.4zm-14.3-2.2c-1.9-.4-3.2-1.9-2.8-3.2s2.4-1.9 4.1-1.5c2 .6 3.3 2.1 2.8 3.4c-.4 1.3-2.4 1.9-4.1 1.3zm-12.5-7.3c-1.5-1.3-1.9-3.2-.9-4.1c.9-1.1 2.8-.9 4.3 .6c1.3 1.3 1.8 3.3 .9 4.1c-.9 1.1-2.8 .9-4.3-.6zm-8.5-10c-1.1-1.5-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3c1.1 1.5 1.1 3.3 0 4.1c-.9 .6-2.6 0-3.7-1.5zm-6.3-8.8c-1.1-1.3-1.3-2.8-.4-3.5c.9-.9 2.4-.4 3.5 .6c1.1 1.3 1.3 2.8 .4 3.5c-.9 .9-2.4 .4-3.5-.6zm-6-6.4c-1.3-.6-1.9-1.7-1.5-2.6c.4-.6 1.5-.9 2.8-.4c1.3 .7 1.9 1.8 1.5 2.6c-.4 .9-1.7 1.1-2.8 .4z"/></svg>
+                            </a>
+                        </span>
+
+
+                        <div class="mycontacts">
+                            <a href="#" class="text-xl"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                            </svg> 
+                            gzmn.h10195@gmail.com </a>
+                            <a href="#" class="text-xl"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                            </svg> 
+                            09958548020 </a>
+                        </div>
+
+                    </div>
+            </div>
+        
+    </div>
+    `
+
+
+
+
+}
